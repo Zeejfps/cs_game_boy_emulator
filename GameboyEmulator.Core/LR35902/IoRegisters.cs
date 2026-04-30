959e8aeea518e6140c8dcce5f9da030abc1796fa
zeejfps <zeejfps@gmail.com>
2026-04-29 22:30:04 -0500

    IoRegisters cleaned up

--- GameboyEmulator.Core/LR35902/IoRegisters.cs
+++ GameboyEmulator.Core/LR35902/IoRegisters.cs
@@ -2,12 +2,6 @@ namespace GameBoyEmulator.Core.LR35902;
 
 public static class IoRegisters
 {
-    public const ushort SerialDataAddress = 0xFF01;
-    public const ushort SerialControlAddress = 0xFF02;
-    public const ushort DividerAddress = 0xFF04;
-    public const ushort TimerCounterAddress = 0xFF05;
-    public const ushort TimerModuloAddress = 0xFF06;
-    public const ushort TimerControlAddress = 0xFF07;
     public const ushort InterruptFlagAddress = 0xFF0F;
     public const ushort InterruptEnableAddress = 0xFFFF;
 }
