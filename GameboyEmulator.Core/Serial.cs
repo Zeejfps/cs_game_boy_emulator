32f45970b4b905d8699e16172f94752be648f385
zeejfps <zeejfps@gmail.com>
2026-04-30 08:28:25 -0500

    Added a Serial that writes to console

---
+++ GameboyEmulator.Core/Serial.cs
@@ -0,0 +1,32 @@
+using GameBoyEmulator.Core.LR35902;
+
+namespace GameBoyEmulator.Core;
+
+public sealed class Serial : ISerial
+{
+    private readonly IInterruptsBus _interrupts;
+    private byte _data;
+    private byte _control;
+
+    public Serial(IInterruptsBus interrupts)
+    {
+        _interrupts = interrupts;
+    }
+
+    public void WriteData(byte value) => _data = value;
+
+    public void WriteControl(byte value)
+    {
+        _control = value;
+        if (value == 0x81)
+        {
+            Console.Write((char)_data);
+            _data = 0xFF;
+            _control = 0x01;
+            _interrupts.Write(InterruptType.Serial);
+        }
+    }
+
+    public byte ReadData() => _data;
+    public byte ReadControl() => (byte)(_control | 0x7E);
+}
