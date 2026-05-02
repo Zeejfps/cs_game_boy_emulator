d8bf0f1ed78b876ae5ab0d74123113cb9027ecfc
zeejfps <zeejfps@gmail.com>
2026-05-02 17:21:24 -0500

    Fixed benchmarks

--- GameboyEmulator.Benchmarks/Micro/NullStubs.cs
+++ GameboyEmulator.Benchmarks/Micro/NullStubs.cs
@@ -13,6 +13,7 @@ internal sealed class NullPpu : IPpu
     public byte ReadOam(ushort address) => 0xFF;
     public void WriteRegister(ushort address, byte value) { }
     public byte ReadRegister(ushort address) => 0xFF;
+    public PpuMode Mode => PpuMode.HBlank;
 }
 
 internal sealed class NullJoypad : IJoypad
